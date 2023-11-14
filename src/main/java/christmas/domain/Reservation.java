430812a100f6500953481a652cfdd3e8998ef665
songjayhyun <thewinner1st@naver.com>
2023-11-14 13:45:50 +0900

    feat(Reservation): Amount 필드 초기화
    - order list의 dish price들을 합해서 초기화

--- src/main/java/christmas/domain/Reservation.java
+++ src/main/java/christmas/domain/Reservation.java
@@ -1,30 +1,48 @@
 package christmas.domain;
 
 import christmas.Order;
-import christmas.domain.category.DrinkCategory;
-import christmas.domain.dish.Dessert;
 import christmas.domain.dish.Dish;
 import christmas.domain.dish.Drink;
 import christmas.exception.DuplicateOrderMenuException;
 import christmas.exception.NoMenuExistsException;
 import christmas.exception.OverMenuSizeAtOnceException;
 import java.util.List;
-import java.util.Optional;
 
 public class Reservation {
 
     private static final int MAX_MENU_SIZE_AT_ONCE = 20;
-    private Amount amount;
     private final List<Order> orders;
+    private Amount amount;
 
     private Reservation(List<Order> orders) {
         validateDuplicate(orders);
         validateMenuSize(orders);
         validateOnlyDrinks(orders);
         this.orders = orders;
+        this.amount = calAmount(orders);
+    }
+
+    private Amount calAmount(List<Order> orders) {
+        int totalAmount = orders.stream()
+                .map(Order::getDish)
+                .mapToInt(Dish::getPrice)
+                .sum();
+        return new Amount(totalAmount);
+    }
+
+    public static Reservation from(List<Order> orders) {
+        return new Reservation(orders);
     }
 
     private void validateOnlyDrinks(List<Order> orders) {
+        int count = countDrinkDish(orders);
+
+        if (count == orders.size()) {
+            throw new OrderedOnlyDrinksException();
+        }
+    }
+
+    private static int countDrinkDish(List<Order> orders) {
         Menu menu = Menu.getInstance();
 
         int count = 0;
@@ -35,10 +53,7 @@ public class Reservation {
                 count++;
             }
         }
-
-        if (count == orders.size()) {
-            throw new OrderedOnlyDrinksException();
-        }
+        return count;
     }
 
     private void validateMenuSize(List<Order> orders) {
@@ -55,10 +70,6 @@ public class Reservation {
         if (orders.size() != orders.stream().map(Order::getDish).distinct().count()) {
             throw new DuplicateOrderMenuException();
         }
-    }
-
-    public static Reservation of(List<Order> orders) {
-        return new Reservation(orders);
     }
 
 }
