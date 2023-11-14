c382137194744df941e362a464c8c1232a7ca9bd
songjayhyun <thewinner1st@naver.com>
2023-11-15 02:21:40 +0900

    test(Amount): Amount 도메인 테스트 코드 작성
    - 총 금액이 10000원 이상이어야 이벤트 적용

---
+++ src/test/java/christmas/domain/AmountTest.java
@@ -0,0 +1,20 @@
+package christmas.domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class AmountTest {
+
+    @DisplayName("총 금액은 10000원 이상이어야 이벤트가 적용된다.")
+    @Test
+    void isEventable() {
+        //given
+        Amount amount = new Amount(5000);
+
+        //when then
+        Assertions.assertThat(amount.isEventable()).isFalse();
+    }
+}
