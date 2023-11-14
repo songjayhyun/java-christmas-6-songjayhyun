8b6e10973a1fda6cd3b2246c86464eaab9d0ee72
songjayhyun <thewinner1st@naver.com>
2023-11-14 13:55:45 +0900

    feat(Dish): equals, hashcode, getter 추가

--- src/main/java/christmas/domain/dish/Dish.java
+++ src/main/java/christmas/domain/dish/Dish.java
@@ -1,9 +1,11 @@
 package christmas.domain.dish;
 
+import java.util.Objects;
+
 public class Dish {
 
-    private String name;
-    private int price;
+    private final String name;
+    private final int price;
 
     public Dish(String name, int price) {
         this.name = name;
@@ -12,6 +14,26 @@ public class Dish {
 
     public boolean contains(String name) {
         return this.name.equals(name);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Dish dish)) {
+            return false;
+        }
+        return price == dish.price && Objects.equals(name, dish.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, price);
+    }
+
+    public int getPrice() {
+        return price;
     }
 
 }
