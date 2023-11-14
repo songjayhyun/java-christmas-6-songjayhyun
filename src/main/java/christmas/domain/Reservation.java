f466e0fdfa5d217d6b6f785fa180b3a0bb77a272
songjayhyun <thewinner1st@naver.com>
2023-11-14 12:07:09 +0900

    feat(Reservation): 메뉴의 사이즈가 20개 이하인지 확인한다

--- src/main/java/christmas/domain/Reservation.java
+++ src/main/java/christmas/domain/Reservation.java
@@ -2,16 +2,26 @@ package christmas.domain;
 
 import christmas.Order;
 import christmas.exception.DuplicateOrderMenuException;
+import christmas.exception.OverMenuSizeAtOnceException;
 import java.util.List;
 
 public class Reservation {
+
+    private static final int MENU_SIZE_AT_ONCE = 20;
     private final Amount amount;
     private final List<Order> orders;
 
     private Reservation(Amount amount, List<Order> orders) {
         validateDuplicate(orders);
+        validateMenuSize(orders);
         this.orders = orders;
         this.amount = amount;
+    }
+
+    private void validateMenuSize(List<Order> orders) {
+        if (orders.size() > MENU_SIZE_AT_ONCE) {
+            throw new OverMenuSizeAtOnceException();
+        }
     }
 
     private void validateDuplicate(List<Order> orders) {
