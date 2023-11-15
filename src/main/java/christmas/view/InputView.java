34feb838ecaec4175a9cd1fcb8e7e8d797cb681e
songjayhyun <thewinner1st@naver.com>
2023-11-15 13:01:14 +0900

    feat(InputView): 날짜와 메뉴,개수를 입력받는다
    - 식당 예상 방문 날짜를 입력받는다
    - 메뉴와 개수를 입력받는다
    - 입력값이 숫자인지 확인
    - 메뉴 형식에 맞는지 확인

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -1,8 +1,21 @@
 package christmas.view;
 
+import christmas.domain.Order;
+import christmas.exception.BlankInputTextException;
+import christmas.exception.ChristmasEventException;
+import christmas.exception.NoNumericException;
 import christmas.io.Reader;
+import christmas.utils.Parser;
+import java.util.List;
+import java.util.function.Supplier;
+import java.util.regex.Pattern;
 
 public class InputView {
+
+    private static final Pattern NUMERIC_PATTERN = Pattern.compile("\\d+");
+    private static final Pattern MENU_PATTERN = Pattern.compile("[,-]");
+    private static final int MENU_PATTERN_SIZE = 2;
+
 
     private final Reader reader;
 
@@ -10,8 +23,70 @@ public class InputView {
         this.reader = reader;
     }
 
+    private int getVisitDate() {
+        return runLoop(() -> {
+            String input = reader.read();
+            validateNumeric(input);
+            return Parser.toInt(input);
+        });
+    }
+
+    private List<Order> getOrders() {
+        return runLoop(() -> {
+            String input = reader.read();
+            validateHasText(input);
+            validateMenuPattern(input);
+            List<String> menus = Parser.toStringList(input);
+            validateOrder(menus);
+            return Parser.toOrderList(menus);
+        });
+    }
+
+    private void validateOrder(List<String> menus) {
+        menus.forEach(menu -> {
+            List<String> orderString = Parser.toOrderString(menu);
+            validateNumeric(orderString.get(1));
+            validateParsedLength(orderString);
+        });
+    }
+
+    private void validateParsedLength(List<String> orderString) {
+        if (orderString.size() != MENU_PATTERN_SIZE) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateNumeric(String input) {
+        if (!NUMERIC_PATTERN.matcher(input).matches()) {
+            throw new NoNumericException();
+        }
+    }
+
+    private void validateMenuPattern(String input) {
+        if (!MENU_PATTERN.matcher(input).matches()) {
+            throw new NoNumericException();
+        }
+    }
+
+    private void validateHasText(String input) {
+        if (input == null || input.isBlank()) {
+            throw new BlankInputTextException();
+        }
+    }
+
+    private <T> T runLoop(Supplier<T> inputFunction) {
+        while (true) {
+            try {
+                return inputFunction.get();
+            } catch (ChristmasEventException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
     public void close() {
         reader.close();
     }
+
 }
 
